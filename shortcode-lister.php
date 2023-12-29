e67580e01ea3f87936fd83f703e9bbfe733d8657
isupercoder <info@fahdmurtaza.com>
2023-12-30 02:49:32 +0500

    Create the file and name and add plugin meta info

---
+++ shortcode-lister.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Shortcode Lister
+ * Plugin URI: http://fahdmurtaza.com/
+ * Description: Lists all shortcodes used in pages, grouped by pages.
+ * Version: 1.0
+ * Author: Fahad Murtaza
+ * Author URI: http://fahdmurtaza.com/
+ */
+
